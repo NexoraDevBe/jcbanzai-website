89edab8c5c0f6f6399b2882417f6526f03791f9b
diekleineroste <laurensrousseau@gmail.com>
2025-09-02 00:22:20 +0200

    CTA component

---
+++ app/components/atom/CallToAction.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+interface Props {
+  onClick?: () => void
+  className?: string
+}
+
+const props = defineProps<Props>()
+
+const handleClick = () => {
+  if (!props.onClick) return
+  props.onClick()
+}
+</script>
+
+<template>
+<button :class="className" @click="handleClick">
+  <slot>
+  </slot>
+</button>
+</template>
+
+<style scoped lang="scss">
+button {
+  display: inline-flex;
+  align-items: center;
+  gap: 1rem;
+  padding: 0.5rem 1rem;
+  border: none;
+  border-radius: 5rem;
+  background-color: var(--accent);
+  color: var(--light);
+  font-size: 1.5rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  cursor: pointer;
+}
+</style>
