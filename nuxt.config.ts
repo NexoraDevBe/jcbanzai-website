0c8daa2eb2542a61ff61ab942009300b288de62d
diekleineroste <laurensrousseau@gmail.com>
2025-09-01 22:15:15 +0200

    sass added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,16 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   ssr: false,
+  css: ['~/assets/scss/main.scss'],
+  app: {
+    head: {
+      title: 'Judoclub Banzai',
+      htmlAttrs: {
+        lang: 'nl',
+      },
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ]
+    }
+  }
 })
