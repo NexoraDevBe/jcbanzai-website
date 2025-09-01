b97320b4795501efb10ff4dc7ba7c0d0825d917e
diekleineroste <laurensrousseau@gmail.com>
2025-09-01 22:17:52 +0200

    app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,17 @@
+<script setup lang="ts">
+useHead({
+  title: 'Judoclub Banzai',
+  meta: [
+    { name: 'description', content: 'My amazing site.' }
+  ],
+  bodyAttrs: {
+    class: 'test'
+  },
+  script: [ { innerHTML: 'console.log(\'Hello world\')' } ]
+})
+</script>
+
 <template>
-  <div>
-    <NuxtPage />
-  </div>
+  <OrganismHeader></OrganismHeader>
+  <NuxtPage />
 </template>
