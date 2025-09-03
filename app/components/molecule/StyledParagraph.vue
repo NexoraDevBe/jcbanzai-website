216eed35b32531b9443cec4e74dbd9bf36932bdd
diekleineroste <laurensrousseau@gmail.com>
2025-09-03 02:40:43 +0200

    styled paragraph component

---
+++ app/components/molecule/StyledParagraph.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+interface Props {
+  right?: boolean
+  maxWidth?: string
+}
+
+withDefaults(defineProps<Props>(), {
+  right: false
+})
+</script>
+
+<template>
+<div class="styled-paragraph" :class="right ? 'right' : 'left'" :style="{maxWidth: maxWidth}">
+  <div v-if="$slots.styleElement" class="style-element">
+    <slot name="styleElement"/>
+  </div>
+  <h3 v-if="$slots.title">
+    <slot name="title"/>
+  </h3>
+  <p v-if="$slots.content">
+    <slot name="content"/>
+  </p>
+</div>
+</template>
+
+<style scoped lang="scss">
+.styled-paragraph {
+  position: relative;
+
+  &.left, &.right {
+    .style-element {
+      position: absolute;
+      top: 50%;
+      z-index: -1;
+    }
+  }
+
+  &.left {
+    .style-element {
+      left: 0;
+      transform: translateX(-50%) translateY(-50%);
+    }
+  }
+
+  &.right {
+    .style-element {
+      right: 0;
+      transform: translateX(50%) translateY(-50%);
+    }
+
+    h3, p {
+      text-align: right;
+    }
+  }
+}
+</style>
