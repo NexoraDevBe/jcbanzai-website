56ced494309a98b0a4cee1999940930ee6808c13
diekleineroste <laurensrousseau@gmail.com>
2025-09-04 18:39:46 +0200

    page transition

--- app/app.vue
+++ app/app.vue
@@ -15,3 +15,15 @@ useHead({
   <NuxtPage/>
   <OrganismAppFooter/>
 </template>
+
+<style scoped lang="scss">
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,6 +13,7 @@ export default defineNuxtConfig({
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       ]
-    }
+    },
+    pageTransition: { name: 'page', mode: 'out-in' }
   }
 })
