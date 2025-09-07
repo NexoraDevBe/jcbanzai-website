798db17f4cd75ee2c5ef3e30700a1b34b3820fc7
diekleineroste <laurensrousseau@gmail.com>
2025-09-07 17:03:56 +0200

    index mobile resposive

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -9,11 +9,14 @@ const cta = () => {
 <template>
   <main id="home-page">
     <section class="hero">
-      <AtomJapaneseText :outline="true" :size="4" :vertical="false">バンザイ</AtomJapaneseText>
+      <AtomJapaneseText :outline="true" :size="4" :vertical="false">バーンザイ</AtomJapaneseText>
       <h1 class="hero-title">Judoclub<br> <span>Banzai</span></h1>
       <AtomCallToAction :on-click="cta">
         Starten?
       </AtomCallToAction>
+      <div aria-hidden="true" class="scroll-indicator">
+        <div class="scroll-dot"/>
+      </div>
     </section>
     <section class="intro">
       <MoleculeStyledParagraph class="paragraph" :max-width="'30rem'">
@@ -37,49 +40,241 @@ const cta = () => {
     display: flex;
     flex-direction: column;
     justify-content: center;
+    align-items: center;
     height: 100dvh;
+    padding-bottom: 3rem;
+
+    $size: 12px;
+
+    .scroll-indicator {
+      position: absolute;
+      bottom: 10vh;
+      display: flex;
+      justify-content: center;
+      height: 80px;
+      width: 16px;
+      border-radius: 1rem;
+      background: linear-gradient(var(--secondary-70), transparent);
+
+      .scroll-dot {
+        width: $size;
+        height: $size;
+        background-color: var(--accent);
+        border-radius: 50%;
+        animation: scrollIndicator 7s infinite cubic-bezier(0.25, 0.40, 0.50, 0.94);
+      }
+    }
 
     .hero-title {
       margin: 0;
+      margin-bottom: 2rem;
       line-height: .8;
-      font-size: 10rem;
+      font-size: 18vw;
       font-weight: 400;
       text-transform: uppercase;
 
       span {
-        font-size: 14rem;
+        font-size: 25vw;
       }
-    }
-
-    button {
-      margin-left: 4rem;
-    }
-
-    &::after {
-      display: block;
-      position: absolute;
-      top: 10vh;
-      right: -20%;
-      content: '';
-      height: 50dvw;
-      max-height: 100%;
-      min-height: 40rem;
-      aspect-ratio: 1;
-      background-color: var(--accent);
-      border-radius: 50%;
-      z-index: -1;
     }
   }
 
   .intro {
-    position: relative;
-    display: flex;
-    height: 2rem;
-
     .paragraph {
-      position: absolute;
-      left: 25vw;
-      bottom: 0;
+      margin-left: var(--page-margin);
+    }
+  }
+}
+
+@media screen and (width >= 40rem) {
+  #home-page {
+    padding: 0;
+
+    .hero {
+      position: relative;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: start;
+      height: 100dvh;
+
+      .scroll-indicator {
+        display: none;
+      }
+
+      button {
+        margin-left: 2rem;
+      }
+    }
+
+    .intro {
+      position: relative;
+      display: flex;
+      height: 2rem;
+
+      .paragraph {
+        position: absolute;
+        left: 25vw;
+        bottom: 0;
+      }
+    }
+  }
+}
+
+@media screen and (width >= 48rem) {
+  #home-page {
+    padding: 0;
+
+    .hero {
+      position: relative;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: start;
+      height: 100dvh;
+
+      .hero-title {
+        margin: 0;
+        line-height: .8;
+        font-size: 8rem;
+        font-weight: 400;
+        text-transform: uppercase;
+
+        span {
+          font-size: 11.2rem;
+        }
+      }
+
+      button {
+        margin-left: 2rem;
+      }
+    }
+
+    .intro {
+      position: relative;
+      display: flex;
+      height: 2rem;
+
+      .paragraph {
+        position: absolute;
+        left: 25vw;
+        bottom: 0;
+      }
+    }
+  }
+}
+
+@media screen and (width >= 64rem) {
+  #home-page {
+    padding: 0;
+
+    .hero {
+      position: relative;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: start;
+      height: 100dvh;
+
+      .hero-title {
+        margin: 0;
+        line-height: .8;
+        font-size: 8rem;
+        font-weight: 400;
+        text-transform: uppercase;
+
+        span {
+          font-size: 11.2rem;
+        }
+      }
+
+      button {
+        margin-left: 2rem;
+      }
+
+      &::after {
+        display: block;
+        position: absolute;
+        top: 10vh;
+        right: -20%;
+        content: '';
+        height: 50dvw;
+        max-height: 100%;
+        min-height: 30rem;
+        aspect-ratio: 1;
+        background-color: var(--accent);
+        border-radius: 50%;
+        z-index: -1;
+      }
+    }
+
+    .intro {
+      position: relative;
+      display: flex;
+      height: 2rem;
+
+      .paragraph {
+        position: absolute;
+        left: 25vw;
+        bottom: 0;
+      }
+    }
+  }
+}
+
+@media screen and (width >= 80rem) {
+  #home-page {
+    padding: 0;
+
+    .hero {
+      position: relative;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      height: 100dvh;
+
+      .hero-title {
+        margin: 0;
+        line-height: .8;
+        font-size: 10rem;
+        font-weight: 400;
+        text-transform: uppercase;
+
+        span {
+          font-size: 14rem;
+        }
+      }
+
+      button {
+        margin-left: 4rem;
+      }
+
+      &::after {
+        display: block;
+        position: absolute;
+        top: 10vh;
+        right: -20%;
+        content: '';
+        height: 50dvw;
+        max-height: 100%;
+        min-height: 40rem;
+        aspect-ratio: 1;
+        background-color: var(--accent);
+        border-radius: 50%;
+        z-index: -1;
+      }
+    }
+
+    .intro {
+      position: relative;
+      display: flex;
+      height: 2rem;
+
+      .paragraph {
+        position: absolute;
+        left: 25vw;
+        bottom: 0;
+      }
     }
   }
 }
