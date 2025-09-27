b9ac0afaa8c1beb724695b46232f1671f1dc7347
diekleineroste <laurensrousseau@gmail.com>
2025-09-27 20:44:06 +0200

    chevron icon

---
+++ app/components/icon/Chevron.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+interface Props {
+  size?: number
+  strokeWidth?: number
+  color?: string
+}
+
+withDefaults(defineProps<Props>(), {
+  size: 24,
+  strokeWidth: 1.5,
+  color: 'secondary'
+})
+</script>
+
+<template>
+  <svg :width="size" :height="size" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M1 7L12 18L23 7" :stroke="`var(--${color})`" :stroke-width="strokeWidth" stroke-linecap="round"/>
+  </svg>
+</template>
