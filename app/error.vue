e4c5a726f35572034c49354a86809135cfa09657
diekleineroste <laurensrousseau@gmail.com>
2025-10-25 23:46:45 +0200

    404 / error page

---
+++ app/error.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+defineProps({
+  error: Object as () => NuxtError,
+})
+
+const handleError = () => clearError({ redirect: '/' })
+</script>
+
+<template>
+  <OrganismAppHeader/>
+  <main id="error-page">
+    <h1>Oeps... {{ error?.statusCode }}</h1>
+    <h3>
+      Deze pagina lijkt van de tatami gevallen.<br>
+      Zelfs de beste judoka’s verliezen soms hun balans.<br>
+      Ga terug naar de homepagina en probeer het opnieuw!
+    </h3>
+    <AtomCallToAction :on-click="handleError">
+      Home
+    </AtomCallToAction>
+  </main>
+  <AtomCursorFollow/>
+</template>
+
+<style scoped lang="scss">
+#error-page {
+
+}
+</style>
