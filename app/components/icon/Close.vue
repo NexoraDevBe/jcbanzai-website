93224b80b34e4ee34d0bf87201fcdde3f32d059a
diekleineroste <laurensrousseau@gmail.com>
2026-04-05 09:44:34 +0200

    UI: added icons

---
+++ app/components/icon/Close.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+interface Props {
+  size?: number
+  strokeWidth?: number
+  color?: string
+}
+
+withDefaults(defineProps<Props>(), {
+  size: 24,
+  strokeWidth: 1.5,
+  color: 'secondary'
+})
+</script>
+
+<template>
+  <svg :width="size" :height="size" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M5 5L12 12M12 12L19 19M12 12L19 5M12 12L5 19" :stroke="`var(--${color})`" :stroke-width="strokeWidth" stroke-linecap="round"/>
+  </svg>
+</template>
---
+++ app/components/icon/SortIndicator.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+interface Props {
+  size?: number
+  strokeWidth?: number
+  color?: string
+}
+
+withDefaults(defineProps<Props>(), {
+  size: 24,
+  strokeWidth: 2,
+  color: 'secondary'
+})
+</script>
+
+<template>
+  <svg :width="size" :height="size" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M3 7H21"  :stroke="`var(--${color})`" :stroke-width="strokeWidth" stroke-linecap="round"/>
+    <path d="M6 12H18" :stroke="`var(--${color})`" :stroke-width="strokeWidth" stroke-linecap="round"/>
+    <path d="M8 17H16" :stroke="`var(--${color})`" :stroke-width="strokeWidth" stroke-linecap="round"/>
+  </svg>
+</template>
