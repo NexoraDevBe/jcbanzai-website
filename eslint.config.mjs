c8db7837f2705897f2b352c23c1f0d4248ae27ff
diekleineroste <laurensrousseau@gmail.com>
2026-05-21 00:21:19 +0200

    eslint .vue config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,25 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import { defineConfig } from 'eslint/config';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+import vue from "eslint-plugin-vue";
+import vueParser from "vue-eslint-parser";
 
 export default defineConfig(
-    {ignores: ['dist', '.output', '.nuxt', '.idea']},
+  { ignores: ["dist", ".output", ".nuxt", ".idea"] },
 
-    eslint.configs.recommended,
-    tseslint.configs.recommended,
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+
+  {
+    files: ["**/*.vue"],
+    languageOptions: {
+      parser: vueParser,
+    },
+    plugins: {
+      vue,
+    },
+    rules: {},
+  },
 );
